afc57e570fad284d0d475a1b27ab31ad9e64c3e9
안수현 <soxyxs4996@gmail.com>
2026-06-14 05:35:47 +0900

    Revert "feat(user): 아이디 및 비밀번호 찾기 기능 구현"

--- actify-user-service/src/main/java/kr/co/actify/user/model/dto/login/LogoutReq.java
+++
@@ -1,9 +0,0 @@
-package kr.co.actify.user.model.dto.login;
-
-import lombok.Data;
-
-@Data
-public class LogoutReq {
-    private String accessToken;
-    private String refreshToken;
-}
--- actify-user-service/src/main/java/kr/co/actify/user/model/vo/UsersStatus.java
+++
@@ -1,8 +0,0 @@
-package kr.co.actify.user.model.vo;
-
-public enum UsersStatus {
-     PENDING_EMAIL,
-     ACTIVE,
-     SUSPENDED,
-     WITHDRAWN
-}
